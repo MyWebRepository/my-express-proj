178700087bdb7f3fefbda652d864e8582aa0f6c9
Home <Home@Homes-iMac.local>
2016-11-19 20:30:51 -0500

    Updated service code.

--- app/users/delete-user.js
+++ app/users/delete-user.js
@@ -4,6 +4,12 @@ var fs = require("fs");
 
 var id = 2;
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.delete('/deleteUser', function (req, res) {
     // First read existing users.
     fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
